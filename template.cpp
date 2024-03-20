6e7fe24ed02ba3decd5e4a42a3055a238a16bb86
jayant-cyber <53310561+jayant-cyber@users.noreply.github.com>
2024-03-21 00:26:01 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> v32;
-#define speed_cin() ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define forn(s,e) for(ll i = s; i < e; i++) // i is i and e - is ending
-
+typedef vector<ll> v64;
+#define speed_cin()                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define forn(s, e) for (ll i = s; i < e; i++) // i is i and e - is ending
 int main(){
     speed_cin();
     //code here 

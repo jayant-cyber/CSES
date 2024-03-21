a61984c55fbbe69b7ffc771bea0f222039afd7b6
jayant-cyber <53310561+jayant-cyber@users.noreply.github.com>
2024-03-21 18:20:11 +0530

    Update Coin_Piles.cpp

--- Coin_Piles.cpp
+++ Coin_Piles.cpp
@@ -23,13 +23,14 @@ int main()
         ll a, b;
         string ans;
         cin >> a >> b;
+        // piles are empty
         if (a == 0 && b == 0)
             ans = "YES";
-        else if (a == 0 || b == 0)
+        else if (a == 0 || b == 0)  // if any one pile is empty
             ans = "NO";
-        else if (a >= (a + b) / 3 && b >= (a + b) / 3)
+        else if (a >= (a + b) / 3 && b >= (a + b) / 3)  // each pile should be greater than 1/3(2+1) of the total
         {
-            if ((a + b) % 3 == 0)
+            if ((a + b) % 3 == 0)  // it should be divisible / 3 (2+1)
                 ans = "YES";
             else
                 ans = "NO";

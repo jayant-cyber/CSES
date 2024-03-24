f7201a597c6dcbe10669f2a8125d89ae7d22b7e9
jayant-cyber <53310561+jayant-cyber@users.noreply.github.com>
2024-03-24 15:50:20 +0530

    Trailing_Zeros

---
+++ Trailing_Zeros.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> v32;
+typedef vector<ll> v64;
+
+ll MOD = 1e9 + 7;
+
+#define speed_cin()                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define forn(s, e) for (ll i = s; i < e; i++) // i is i and e - is ending
+ 
+int main()
+{
+    speed_cin();
+    ll n,count=0;
+    cin>>n;
+    for (int i = 5; n / i >= 1; i *= 5)
+        count += n / i;
+ 
+    cout<< count;
+    return 0;
+}
